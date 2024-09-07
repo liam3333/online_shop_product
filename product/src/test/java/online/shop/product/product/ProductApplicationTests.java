227596d47c2a52c57b10f71fb68d6a2d328914d5
liam3333 <90696675+liam3333@users.noreply.github.com>
2024-09-07 19:57:29 +0700

    init gateway, inventory, product, user

---
+++ product/src/test/java/online/shop/product/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package online.shop.product.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
